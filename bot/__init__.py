d6f6574bf0fe08146ae7ae819734b47aa1780c1d
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-08-30 20:42:03 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -145,7 +145,7 @@ if DEFAULT_UPLOAD != 'rc':
 
 DOWNLOAD_DIR = environ.get('DOWNLOAD_DIR', '')
 if len(DOWNLOAD_DIR) == 0:
-    DOWNLOAD_DIR = '/data'
+    DOWNLOAD_DIR = '/tmp'
 elif not DOWNLOAD_DIR.endswith("/"):
     DOWNLOAD_DIR = f'{DOWNLOAD_DIR}/'
 
